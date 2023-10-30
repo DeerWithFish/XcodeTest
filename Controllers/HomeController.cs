25902a2b7b9a7dd2f287578892e5523632f9b9db
xingyang <xingyang@DESKTOP-CI7T913>
2023-10-30 14:20:43 +0800

    添加项目文件。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using XcodeTest.Models;
+using XCode;
+using Newtonsoft.Json.Linq;
+using XCode.Membership;
+using XCode.DataAccessLayer;
+using ConnName;
+using NewLife.Serialization;
+using System.Collections.Generic;
+
+namespace XcodeTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GetUserList()
+        {
+
+            var user2 = DtUser.FindAll();
+
+
+
+            return Json(new
+            {
+                //Code = 0,
+                //Msg = "ok",
+                list = user2.ToJson(),
+            });
+        }
+    }
+}
---
+++ wwwroot/js/Home/Index.js
@@ -0,0 +1,34 @@
+﻿
+var app = new Vue({
+    el: '#app',
+    data: {
+        visible: false
+    },
+    mounted: function () {
+        let vue = this;
+
+        this.onload();//需要触发的函数
+    },
+    methods: {
+        onload: function () {
+            this.GetUserList();
+        },
+        GetUserList: function () {
+
+
+            axios.get('../Home/GetUserList')
+                .then(response => {
+                    // 处理响应数据
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    // 处理错误
+                    console.log(error);
+                });
+
+        },
+        show: function () {
+            this.visible = true;
+        }
+    }
+})
